c94b6fef927cc5bde3a43d9e218fc3a23e0efc85
filips92 <filsmu@wp.pl>
2016-01-16 16:20:45 +0100

    Location controller with first function for calculating the distance between two nodes

---
+++ VRPTW/Controller/LocationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRPTW.Model;
+
+namespace VRPTW.Controller
+{
+    public class LocationController
+    {
+        public double DistanceBetweenLocations(Node location1, Node location2)
+        {
+            return Math.Sqrt(Math.Pow((location2.X - location1.X), 2) + Math.Pow((location2.Y - location1.Y), 2));
+        }
+    }
+}

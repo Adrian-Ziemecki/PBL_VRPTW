e5c6519b2e29cdab5bbfcee1d230c0ae989299e7
filips92 <filsmu@wp.pl>
2016-01-16 15:51:06 +0100

    Node model

---
+++ VRPTW/Model/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRPTW.Model
+{
+    public class Node
+    {
+        public int CustomerNr { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Demand { get; set; }
+        public int ReadyTime { get; set; }
+        public int DueDate { get; set; }
+        public int Service { get; set; }        
+        public List<int> Connections { get; set; }
+
+        public Node()
+        {
+            Connections = new List<int>();
+        }
+    }
+}

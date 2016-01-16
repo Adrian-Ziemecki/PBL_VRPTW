2741d2310925e684685114cfc2ae0c9defffa52d
filips92 <filsmu@wp.pl>
2016-01-16 16:03:39 +0100

    Map model

---
+++ VRPTW/Model/Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRPTW.Model
+{
+    public class Map
+    {
+        public List<Node> Locations { get; set; }
+        public int NumberOfVehicles { get; set; }
+        public int VehicleCapacity { get; set; }
+
+        public Map()
+        {
+            Locations = new List<Node>();   
+        }
+    }
+}
